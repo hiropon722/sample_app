79d85bc56465507ea1f47d10851fa54577f505e7
hiropon722 <liera722@gmail.com>
2023-06-20 07:40:33 +0000

    editページの作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.authenticated?(session[:session_token])
+      if user && session[:session_token] == user.session_token
         @current_user = user
       end
        @current_user ||= User.find_by(id: session[:user_id])
@@ -46,6 +46,11 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
 
 end
